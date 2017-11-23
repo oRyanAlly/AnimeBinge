2c550cdb25cdef507c46411d24a2031fd226e534
oRyanAlly <xRyanAlly@gmail.com>
2017-11-23 11:41:25 -0500

    Update

--- AnimeBinge V2/app/src/main/java/com/animebinge/rally0565/animebinge/AnimePage.java
+++ AnimeBinge V2/app/src/main/java/com/animebinge/rally0565/animebinge/AnimePage.java
@@ -1,6 +1,7 @@
 package com.animebinge.rally0565.animebinge;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
@@ -11,9 +12,15 @@ import android.widget.Toast;
 
 public class AnimePage extends AppCompatActivity {
 
+    DatabaseHelper dhHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anime_page);
+
+        Intent pastedIntent = getIntent();
+        int nPos = pastedIntent.getIntExtra("position", 0);
+        Cursor anime = dhHelper.getAnime(nPos);
+        Toast.makeText(this, anime.getString(1), Toast.LENGTH_SHORT).show();
     }
 }
