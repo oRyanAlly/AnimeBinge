6ac6a6c64eef4110fedc7b3d5c712c48de6432c4
oRyanAlly <xRyanAlly@gmail.com>
2018-01-01 19:30:40 -0500

    Completed Implicit Intents
    Used intents to similiar applications

--- AnimeBinge V3/app/src/main/java/com/animebinge/rally0565/animebinge/Settings.java
+++ AnimeBinge V3/app/src/main/java/com/animebinge/rally0565/animebinge/Settings.java
@@ -1,13 +1,41 @@
 package com.animebinge.rally0565.animebinge;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Settings extends AppCompatActivity {
+public class Settings extends AppCompatActivity implements View.OnClickListener {
 
+    Button btnCrunchy, btnFunimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+        btnCrunchy = findViewById(R.id.btnCrunchy);
+        btnFunimation = findViewById(R.id.btnFunimation);
+
+        btnCrunchy.setOnClickListener(this);
+        btnFunimation.setOnClickListener(this);
+    }
+
+    //https://stackoverflow.com/questions/11753000/how-to-open-the-google-play-store-directly
+    // -from-my-android-application
+    @Override
+    public void onClick(View v) {
+        if(btnCrunchy.getId() == v.getId()) {
+            Intent iCrunchyRoll = new Intent(Intent.ACTION_VIEW);
+            iCrunchyRoll.setData(Uri.parse("market://details?id=com.crunchyroll.crunchyroid"));
+            startActivity(iCrunchyRoll);
+        }
+        if(btnFunimation.getId() == v.getId()) {
+            Intent iFunimation = new Intent(Intent.ACTION_VIEW);
+            iFunimation.setData(Uri.parse("market://details?id=com.Funimation.FunimationNow"));
+            startActivity(iFunimation);
+        }
     }
 }
